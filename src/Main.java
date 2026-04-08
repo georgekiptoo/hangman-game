be24915af5ad2bcc965d6263a82a5d9379799f2b
georgekiptoo <limogeorge14@gmail.com>
2026-04-08 13:53:38 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import ui.HangmanFrame;
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(HangmanFrame::new);
-    }
-}
